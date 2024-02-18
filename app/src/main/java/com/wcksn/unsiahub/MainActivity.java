76eacc401acdf8987b33be25689cea47c5b63ae8
arifww <arif.w.w@gmail.com>
2024-02-18 14:08:02 +0700

    push UAS branch

--- app/src/main/java/com/wcksn/unsiahub/MainActivity.java
+++ app/src/main/java/com/wcksn/unsiahub/MainActivity.java
@@ -1,14 +1,80 @@
 package com.wcksn.unsiahub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText txtNIM, txtPassword;
+    private Button btnReg, btnLogin;
+    private DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReg = findViewById(R.id.btnReg);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtNIM = findViewById(R.id.txtNIM);
+        txtPassword = findViewById(R.id.txtPassword);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(register);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nim = txtNIM.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                db = FirebaseDatabase.getInstance().getReference("users");
+
+                if (nim.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Email Atau Password Salah",Toast.LENGTH_SHORT).show();
+                }else{
+                    db.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.child(nim).exists()){
+                                if (snapshot.child(nim).child("password").getValue(String.class).equals(password)){
+                                    Toast.makeText(getApplicationContext(), "Login Berhasil",Toast.LENGTH_SHORT).show();
+                                    Intent login = new Intent(getApplicationContext(), ProfileActivity.class);
+                                    startActivity(login);
+                                }
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Data Belum Terdaftar",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/wcksn/unsiahub/RegisterActivity.java
+++ app/src/main/java/com/wcksn/unsiahub/RegisterActivity.java
@@ -1,14 +1,67 @@
 package com.wcksn.unsiahub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText txtEmail, txtName, txtNIM, txtPassword, txtPassword2;
+    private Button btnRegister;
+    private DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtNIM = findViewById(R.id.txtNIM);
+        txtName = findViewById(R.id.txtName);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtPassword2 = findViewById(R.id.txtPassword2);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        db = FirebaseDatabase.getInstance().getReferenceFromUrl("https://unsiahub-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = txtEmail.getText().toString();
+                String name = txtName.getText().toString();
+                String nim = txtNIM.getText().toString();
+                String password = txtPassword.getText().toString();
+                String password2 = txtPassword2.getText().toString();
+
+                if (email.isEmpty() || name.isEmpty() || nim.isEmpty() || password.isEmpty() || password2.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Data belum lengkap!", Toast.LENGTH_SHORT).show();
+                //    } else if (password.equals(password2)) {
+                //        Toast.makeText(getApplicationContext(),"Password tidak sama!", Toast.LENGTH_SHORT).show();
+                }else {
+                    db = FirebaseDatabase.getInstance().getReference("users");
+                    db.child(nim).child("nim").setValue(nim);
+                    db.child(nim).child("email").setValue(email);
+                    db.child(nim).child("name").setValue(name);
+                    db.child(nim).child("password").setValue(password);
+
+                    Toast.makeText(getApplicationContext(),"Register berhasil!", Toast.LENGTH_SHORT).show();
+                    Intent register = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(register);
+
+                }
+
+
+            }
+        });
     }
 }
